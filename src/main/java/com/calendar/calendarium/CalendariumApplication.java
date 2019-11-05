b9d4ed7a87bff7b7f26ec69ecc53665a1b095f49
psheemas <kulakowski.przemyslaw@gmail.com>
2019-11-05 09:41:53 +0100

    initial commit

---
+++ src/main/java/com/calendar/calendarium/CalendariumApplication.java
@@ -0,0 +1,13 @@
+package com.calendar.calendarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendariumApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/calendar/calendarium/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.calendar.calendarium.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/v1/calendar")
+public class MainController {
+}
